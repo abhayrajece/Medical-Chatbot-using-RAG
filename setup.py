438d8ceca1ecc66a8f0fdd9e516560fbea332b43
Abhay Raj <abhayraj.ece@gmail.com>
2026-04-19 19:51:11 +0530

    Requirements Added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Abhay Raj",
+    author_email="abhay.raj.ece@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
